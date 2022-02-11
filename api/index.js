2474c46bb0de8b827465a2bf98ad379f3d2960db
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2022-02-11 18:10:24 +0100

    Created http and https api channels server for requests

---
+++ api/index.js
@@ -0,0 +1,47 @@
+// Imports without needed variable
+require('dotenv').config()
+
+// Imports with needed variable
+const express = require('express')
+const cors = require("cors")
+const socketio = require('socket.io')
+const https = require('https')
+const http = require('http')
+const mongoose = require('mongoose')
+const config = require('config')
+
+// Express and socketio initialization for http and https requests
+var app = express()
+var server = https.createServer({
+    key: '',
+    cert: '',
+    ca: ''
+}, app)
+var serverNotSecure = http.createServer(app)
+var io = socketio(server)
+var ioNotSecure = socketio(serverNotSecure)
+
+// Checking if private key of the server is present
+if (!config.get('PrivateKey')) {
+    console.error('FATAL ERROR: PrivateKey is not defined.')
+    process.exit(1)
+}
+
+// Connect to database
+mongoose.connect('mongodb://localhost/MythWars', { useNewUrlParser: true,  useUnifiedTopology: true})
+.then(() => console.log('Now connected to MongoDB!'))
+.catch(err => console.error('Something went wrong', err))
+
+// Use Cors and parse all requests to be a json string
+app.use(cors())
+app.use(express.json())
+
+// Run servers
+
+// HTTPS
+const PORT = process.env.PORT
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+//HTTP
+const PORT_NOT_SECURE = process.env.PORT_NOT_SECURE
+serverNotSecure.listen(PORT_NOT_SECURE, () => console.log(`Server running on port ${PORT_NOT_SECURE}`))
