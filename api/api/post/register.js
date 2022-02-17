4170bf2de93562b0701c13afd023c25d89afc763
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2022-02-17 15:35:17 +0100

    return useless log

--- api/api/post/register.js
+++ api/api/post/register.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/', async (req, res) => {
-    console.log(req.body)
     return res.status(200).send({status: 'OK'})
 })
 
