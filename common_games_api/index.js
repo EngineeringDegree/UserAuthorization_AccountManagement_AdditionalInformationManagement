a135d6de5eec406a27848d6d35ce48011d11afb3
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2022-06-18 13:39:15 +0200

    changed naming and added next api for modifiers and so on.

---
+++ common_games_api/index.js
@@ -0,0 +1,48 @@
+// Imports without needed variable
+require('dotenv').config()
+
+// Imports with needed variable
+const express = require('express')
+const cors = require("cors")
+const https = require('https')
+const http = require('http')
+const mongoose = require('mongoose')
+const fs = require('fs')
+const config = require('config')
+
+// Express and socketio initialization for http and https requests
+var app = express()
+
+// Comment on the server this snippet and uncomment below one
+var server = https.createServer({
+    key: fs.readFileSync(process.env.KEY, 'utf8'),
+    cert: fs.readFileSync(process.env.CERT, 'utf8'),
+    ca: fs.readFileSync(process.env.CA, 'utf8')
+}, app)
+
+var serverNotSecure = http.createServer(app)
+
+// Checking if private key of the server is present
+if (!config.get('PrivateKey')) {
+    console.error('FATAL ERROR: PrivateKey is not defined.')
+    process.exit(1)
+}
+
+// Connect to database
+mongoose.connect(process.env.DATABASE_CONNECTION_STRING, { useNewUrlParser: true,  useUnifiedTopology: true})
+.then(() => console.log('Now connected to MongoDB!'))
+.catch(err => console.error('Something went wrong', err))
+
+// Use Cors and parse all requests to be a json string
+app.use(cors())
+app.use(express.json())
+
+// Run servers
+
+// HTTPS
+const PORT = process.env.PORT
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+//HTTP
+const PORT_NOT_SECURE = process.env.PORT_NOT_SECURE
+serverNotSecure.listen(PORT_NOT_SECURE, () => console.log(`Server running on port ${PORT_NOT_SECURE}`))
