eb2ca0c3082ae7ca46a96eb54dacb0c31e825d3e
MadejaMaciej <48794984+MadejaMaciej@users.noreply.github.com>
2022-06-30 22:28:43 +0200

    Deleted front end registration from auth api

--- auth_api/api/get/views/registeredView.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// Middleware which sends registered page with breadcrumbs
-router.get('/', async (req, res) => {
-    return res.status(200).render('pages/registered')
-})
-
-module.exports = router
--- auth_api/api/get/views/signInView.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-// Middleware which sends signin page with breadcrumbs
-router.get('/', async (req, res) => {
-    return res.status(200).render('pages/signin')
-})
-
-module.exports = router
